62ac69652068663a4e79c969c84984ed24889bab
sagdish <forsi1@yahoo.com>
2019-12-10 23:00:22 +0500

    return token on loging and registering new user

--- routes-models/users-router.js
+++ routes-models/users-router.js
@@ -3,6 +3,8 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const Users = require('../routes-models/users-model');
+const private = require('../config/private')
+// console.log(private);
 
 // for endpoints beginning with /api/users
 router.post('/register', (req, res) => {
@@ -12,7 +14,8 @@ router.post('/register', (req, res) => {
 
   Users.add(user)
     .then(saved => {
-      res.status(201).json(saved);
+      const token = genToken(saved);
+      res.status(201).json({ newUser: saved, token });
     })
     .catch(error => {
       res.status(500).json(error);
@@ -21,22 +24,18 @@ router.post('/register', (req, res) => {
 
 router.post('/login', (req, res) => {
   let { username, password } = req.body;
-  // console.log(username)
+  console.log(username)
 
   Users.findBy({ username })
     .first()
     .then(user => {
+      console.log('search results: ', user)
       if (user && bcrypt.compareSync(password, user.password)) {
-        const payload = {
-          subject: 'authorisation',
-          exp: Math.floor(Date.now() / 1000) + (60 * 60),
-          // exp: '1h',
-        };
-        const token = jwt.sign(payload, 'shhhhh');
+        const token = genToken(user);
 
         res.status(200).json({
           message: `Welcome ${user.username}!`,
-          token
+          yourToken: token
         });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
@@ -46,5 +45,20 @@ router.post('/login', (req, res) => {
       res.status(500).json({error: `database error, or wrong db set up, ${error}`});
     });
 });
+
+function genToken(user) {
+  const payload = {
+    userId: user.id,
+    username: user.username,
+    department: user.department, 
+  }
+  
+  const options = {
+    expiresIn: '3h',
+  }
+
+  const token = jwt.sign(payload, private.jwtSecret, options);
+  return token;
+}
 
 module.exports = router;
